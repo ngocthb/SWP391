00a2e02b69449f627f58edc9178a5ca50c2613e2
Phạm Tấn Phát <tanphat120104@gmail.com>
2024-09-25 07:55:53 +0700

    header manager

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AllRoutes from "./Components/AllRoutes";
+import Header from "./Components/Manager/Header/Header";
 
 function App() {
   return (
     <>
-      <AllRoutes />
+      {/* <AllRoutes /> */}
+      <Header/>
     </>
   );
 }
